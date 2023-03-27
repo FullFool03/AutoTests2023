a20cf2d051d68178fd20ad28dbb6585e3692e508
FullFool03 <Ameiza_228@list.ru>
2023-03-27 17:55:47 +0300

    Second HW done LoadableComponent and Chain of invocations in previous tests, made some updates, added User.java for better registration started other tests in ProfileTests.java, added more annotations in BaseTest, i am not sure if it is ok for the task, waiting for response

---
+++ src/test/java/ProfileTests.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.byXpath;
+
+public class ProfileTests {
+    private final By FRIENDS_FILD = byXpath("//*[@id=\"hook_Block_Navigation\"]/div/div/div[3]/a/div");
+    private final static String BASE_URL = "https://ok.ru";
+    private final static String RIGHT_LOGIN = "botS23AT18";
+    private final static String RIGHT_PASSWORD = "autotests2023";
+    private static By element = byXpath("//input[@name='st.email']");
+    private final static User user = new User(RIGHT_LOGIN, RIGHT_PASSWORD);
+
+    public ProfileTests(){
+        MainPage page = new MainPage(BASE_URL, element);
+        page
+                .setEmailField(user)
+                .setPasswordField(user)
+                .pressEnter();
+        page.getMainPage().shouldBe(Condition.visible);
+    }
+
+    @Test
+    public void friends(){
+        MainPage page = new MainPage();
+        page.friendsEnter().click();
+        page.getShcoolFreindsField().shouldBe(Condition.visible);
+//        page.photoeEnter();
+    }
+
+}
